cfff0684292ff20af4a4ddbf57496c44b0ea4bdf
love-san <anubhav0603@gmail.com>
2018-06-26 18:49:49 +0530

    refactor fixtures page

--- resourses/js/matches_working.js
+++ resourses/js/matches_working.js
@@ -1,26 +1,11 @@
-// var today = new Date();
-// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// var dateTime = date+' '+time;
-// console.log(today.getDate() + " " + today.getMonth() + " " + today.getFullYear());
-
+/*jshint esversion: 6 */
+// ======================GLOBALS=======================
 var url2 = "https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json";
 var unknown_image = "https://d30y9cdsu7xlg0.cloudfront.net/png/513446-200.png";
-function Match(name, home_team, away_team, date, stadium, home_result, away_result, finished, match_day) {
-    this.name           = name;
-    this.home_team      = home_team;
-    this.away_team      = away_team;
-    this.date           = date;
-    this.stadium        = stadium;
-    this.home_result    = home_result;
-    this.away_result    = away_result;
-    this.finished       = (finished === true);
-    this.match_day      = match_day;
-}
-
 var allMatches = [];
 var allData = {};
 
+// ===========================MAJOR FUNCTIONING===========================
 axios.get(url2)
 .then(function(res) {
     allData = res.data;
@@ -29,72 +14,27 @@ axios.get(url2)
         groups[data].matches.forEach(function(match) {
             var a_match = new Match(match.name, match.home_team, match.away_team, match.date, match.stadium, match.home_result, match.away_result, match.finished, match.matchday);
             allMatches[Number(a_match.name)] = a_match;
-            console.log(a_match);
+        });
+    });
+    var knockout = res.data.knockout;
+    Object.keys(knockout).forEach(function(data){
+        knockout[data].matches.forEach(function(match) {
+            var a_match = new Match(match.name, match.home_team, match.away_team, match.date, match.stadium, match.home_result, match.away_result, match.finished, match.matchday);
+            allMatches[Number(a_match.name)] = a_match;
         });
     });
 })
-.then(function(){
-    console.log(allMatches);
-    // console.log(allData.teams);
+.then(function() {
     var temp_day;
     for(var i = 1; i < allMatches.length; i++) {
         var date = allMatches[i].date;
-        var match_day = String(date).slice(8, 10) + ' ' + String(date).slice(5, 7);
-        var match_time = String(date).slice(11, 19);
-        if(match_day[match_day.length-1] == 6) {
-            match_day = match_day.slice(0, 3) + "June";
-        }
-        if(temp_day != match_day) {
-            var details_div = document.createElement('div');
-            details_div.className = 'date';
-            details_div.innerHTML = '<p class="matches_header">' + match_day + '</p>';
-            document.querySelector(".matches-container").appendChild(details_div);
-            temp_day = match_day;
-        }
-        var team_div = document.createElement('div');
+        var match_date = returnDate(date);
+        temp_day = createDateDiv(temp_day, match_date);
+
         if(allMatches[i].finished) {
-            team_div.className = 'match';
-            // team_div.innerHTML = '<p class="home_team">' +  + '</p>';
-            var temp_div1 = document.createElement('div');
-            temp_div1.className = 'team';
-            temp_div1.innerHTML = '<p class="team-name">' + allData.teams[Number(allMatches[i].home_team)-1].name + '</p>';
-            temp_div1.innerHTML += '<img class="team-flag" src="' + allData.teams[Number(allMatches[i].home_team)-1].flag + '">';
-            team_div.appendChild(temp_div1);
-
-            var center_div = document.createElement('div');
-            center_div.className = 
-
-            team_div.innerHTML += '<p class="home_result">' + allMatches[i].home_result + '</p>';
-
-            team_div.innerHTML += '<p>-</p>';
-
-            team_div.innerHTML += '<p class="away_result">' + allMatches[i].away_result + '</p>';
-
-            var temp_div2 = document.createElement('div');
-            temp_div2.className = 'team';
-            temp_div2.innerHTML = '<p class="team-name">' + allData.teams[Number(allMatches[i].away_team)-1].name + '</p>';
-            temp_div2.innerHTML += '<img class="team-flag" src="' + allData.teams[Number(allMatches[i].away_team)-1].flag + '">';
-
-            team_div.appendChild(temp_div2);
-            document.querySelector(".matches-container").appendChild(team_div);
+            createDoneMatch(i, match_date);
         } else {
-            team_div.className = 'match';
-            // team_div.innerHTML = '<p class="home_team">' +  + '</p>';
-            var temp_div1 = document.createElement('div');
-            temp_div1.className = 'team';
-            temp_div1.innerHTML = '<p class="team-name">' + allData.teams[Number(allMatches[i].home_team)-1].name + '</p>';
-            temp_div1.innerHTML += '<img class="team-flag" src="' + allData.teams[Number(allMatches[i].home_team)-1].flag + '">';
-            team_div.appendChild(temp_div1);
-
-            team_div.innerHTML += '<p>VS</p>';
-
-            var temp_div2 = document.createElement('div');
-            temp_div2.className = 'team';
-            temp_div2.innerHTML = '<p class="team-name">' + allData.teams[Number(allMatches[i].away_team)-1].name + '</p>';
-            temp_div2.innerHTML += '<img class="team-flag" src="' + allData.teams[Number(allMatches[i].away_team)-1].flag + '">';
-
-            team_div.appendChild(temp_div2);
-            document.querySelector(".matches-container").appendChild(team_div);
+            createNotDoneMatch(i, match_date);
         }
     }
 })
@@ -108,21 +48,103 @@ axios.get(url2)
     }
 });
 
-// var url = "https://fifa-2018-apis.herokuapp.com/fifa/fixtures";
-// axios.get(url)
-// .then(function(res) {
-//     var i = 1;
-//     console.log(res.data.data);
-//         Object.keys(res.data.data.group_stages).forEach(function(day, index) {
-//             console.log(day);
-//         });
-// })
-// .catch(function(err) {
-//     if (err.response) {
-//         console.log("PROBLLEM WITH RESPONSE!", err.response.status);
-//     } else if (err.request) {
-//         console.log("PROBLEM WITH REQUEST!");
-//     } else {
-//         console.log("ERROR", err.message);
-//     }
-// });
+// ===============================HELPRE FUNCTIONS======================================
+
+// Constructor function for making a match object
+function Match(name, home_team, away_team, date, stadium, home_result, away_result, finished, match_day) {
+    this.name           = name;
+    this.home_team      = home_team;
+    this.away_team      = away_team;
+    this.date           = date;
+    this.stadium        = stadium;
+    this.home_result    = home_result;
+    this.away_result    = away_result;
+    this.finished       = (finished === true);
+    this.match_day      = match_day;
+}
+
+function returnDate(date) {
+    var match_day = String(date).slice(8, 10) + ' ' + String(date).slice(5, 7);
+    var match_time = (Number(String(date).slice(11, 13))) + String(date).slice(13 , 19);
+    if(match_day[match_day.length-1] == 6) {
+        match_day = match_day.slice(0, 3) + "June";
+    } else if(match_day[match_day.length-1] == 7) {
+        match_day = match_day.slice(0, 3) + "July";
+    }
+    var match_date = {
+        match_day: match_day,
+        match_time: match_time
+    };
+    return match_date;
+}
+
+function createDateDiv(temp_day, match_date) {
+    if(temp_day != match_date.match_day) {
+        var details_div = document.createElement('div');
+        details_div.className = 'date';
+        details_div.innerHTML = '<p class="matches_header">' + match_date.match_day + '</p>';
+        document.querySelector(".matches-container").appendChild(details_div);
+        temp_day = match_date.match_day;
+    }
+    return temp_day;
+}
+
+function returnTeam(team) {
+    let temp_div = document.createElement('div');
+    temp_div.className = 'team';
+    console.log(String(team));
+    if(String(team)[0] != 'r' && String(team)[0] != 'w') {
+        temp_div.innerHTML = '<p class="team-name">' + allData.teams[Number(team)-1].name + '</p>';
+        temp_div.innerHTML += '<img class="team-flag" src="' + allData.teams[Number(team)-1].flag + '">';
+    } else {
+        temp_div.innerHTML = '<p class="team-name">' + team + '</p>';
+        temp_div.innerHTML += '<img class="team-flag" src="' + unknown_image + '">';
+        console.log(allMatches[i].home_team);
+    }
+    return temp_div;
+}
+
+function createDoneMatch (i, match_date) {
+    var team_div = document.createElement('div');
+    team_div.className = 'match';
+    let temp_div1 = returnTeam(allMatches[i].home_team);
+    team_div.appendChild(temp_div1);
+
+    var center_div = document.createElement('div');
+    center_div.className = 'centeral';
+    center_div.innerHTML = '<p class="time">' + match_date.match_time + '</p>';
+
+    var lower_center_div = document.createElement('div');
+    lower_center_div.className = 'scores';
+    lower_center_div.innerHTML += '<p class="home_result">' + allMatches[i].home_result + '</p>';
+    lower_center_div.innerHTML += '<p>-</p>';
+    lower_center_div.innerHTML += '<p class="away_result">' + allMatches[i].away_result + '</p>';
+    center_div.appendChild(lower_center_div);
+    team_div.appendChild(center_div);
+
+    let temp_div2 = returnTeam(allMatches[i].away_team);
+    team_div.appendChild(temp_div2);
+    document.querySelector(".matches-container").appendChild(team_div);
+}
+
+function createNotDoneMatch (i, match_date) {
+    var team_div = document.createElement('div');
+    team_div.className = 'match';
+    let temp_div1 = returnTeam(allMatches[i].home_team);
+    team_div.appendChild(temp_div1);
+
+    let center_div = document.createElement('div');
+    center_div.className = 'centeral';
+    center_div.innerHTML = '<p class="time">' + match_date.match_time + '</p>';
+
+    let lower_center_div = document.createElement('div');
+    lower_center_div.className = 'scores';
+    lower_center_div.innerHTML = '<p>VS</p>';
+    center_div.appendChild(lower_center_div);
+    team_div.appendChild(center_div);
+
+    // console.log(String(allMatches[i].away_team));
+    var temp_div2 = returnTeam(allMatches[i].away_team);
+    team_div.appendChild(temp_div2);
+    document.querySelector(".matches-container").appendChild(team_div);
+}
